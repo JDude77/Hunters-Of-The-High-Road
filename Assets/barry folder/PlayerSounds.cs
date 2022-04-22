3978fdddaed8bb8324cced425fdf00769c33ddbf
Soap Forrest <49860657+Soap187@users.noreply.github.com>
2022-04-22 18:07:53 +0100

    meta files in stash pushed

---
+++ Assets/barry folder/PlayerSounds.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSounds : MonoBehaviour
+{
+    [SerializeField]
+    private AK.Wwise.Event footstepsEvent;
+    private void PlayFootstepSound()
+    {
+
+    }
+}
